520bf765292f8d5ffd762e4af8725dd248244646
Liza Garg <lizagarg5@gmail.com>
2025-01-02 22:22:13 +0530

    wrappers

--- react-experiments/src/App.jsx
+++ react-experiments/src/App.jsx
@@ -1,16 +1,24 @@
+import React, {useState} from 'react'
 function App() {
-
-  return (
-    <>
-      <Header title="Welcome to React"/>
-      <Header title="Welcome to React"/>
-    </>
-  )
+  return <div>
+  <CardWrapper>
+    <TextComponent />
+  </CardWrapper>
+  <CardWrapper>
+    hello there!
+  </CardWrapper>
+  </div>
 }
 
-function Header({title}) { //obj destructing
+function CardWrapper({children}) {
+ return <div style={{border: '2px solid black', padding: '10px', margin: '10px'}}>
+  {children}
+ </div>
+}
+
+function TextComponent() {
   return <div>
-    {title}
+    hello from text component
   </div>
 }
 export default App
